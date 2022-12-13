c66ec1a6359cc12d909286e9935d9e6e61ae7c93
Florian Rohm <rohm.florian@googlemail.com>
2022-12-13 14:59:08 +0100

    day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input.txt");
+    let mut calories = input.split("\n\n").into_iter().map(sum_calories).collect::<Vec<_>>();
+    let maximum_calories = calories.iter().max().expect("there should be at least one line");
+
+    println!("The maximum calories an elve is carrying is {}", maximum_calories);
+    calories.sort();
+    calories.reverse();
+    let top_three_calories: usize = calories[0..3].iter().sum();
+    println!("The calories the top three elves are carrying is {}", top_three_calories);
+
+}
+
+fn sum_calories(input: &str) -> usize {
+    input.lines().map(|line| line.parse::<usize>().expect("should contain a parsable line")).sum()
 }
